8417d2528a05766c6aa3d7bc77a95763076571d9
dhmeson <dhmesonaraujo123@gmail.com>
2024-05-08 11:20:51 -0300

    add test

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,7 @@ export class ExpressServer {
     this.app.use(path, routes);
   }
   listen() {
+    this.app.get('/', (req, res) => res.send('test'));
     this.app.listen(this.PORT, () => {
       console.log(`Server is running on http://localhost:${this.PORT}`);
     });
